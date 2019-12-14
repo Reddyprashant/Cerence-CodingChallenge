68151ad865539f95ec97ba4301959fda1def5cad
Reddyprashant <reddyprashant92@gmail.ocm>
2019-12-14 12:27:51 -0500

    Creating fibonaccigenerator project

---
+++ fibonacciGenertaor/src/main/java/com/cernece/fibonacciGenertaor/FibonacciGenertaorApplication.java
@@ -0,0 +1,13 @@
+package com.cernece.fibonacciGenertaor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FibonacciGenertaorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FibonacciGenertaorApplication.class, args);
+	}
+
+}
